847e47e183b25be95604599366d42d55c5989543
Khalal Walker <khalal.walker@gmail.com>
2019-01-25 15:35:38 -0800

    add list functionality and styling

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,3 +1,4 @@
+import { HEROES } from '../mock-heroes';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
@@ -7,11 +8,14 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.styl']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
   constructor() {}
 
   ngOnInit() {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(hero);
+  }
 }
