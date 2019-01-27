7c741b982adad0c622b4c80c0004fd2c52ae6263
Khalal Walker <khalal.walker@gmail.com>
2019-01-26 18:11:05 -0800

    update app with dashboards and routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HeroService } from './../hero.service';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,25 +10,12 @@ import { HeroService } from './../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
+
   constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes();
   }
-
-  onSelect(hero: Hero): void {
-    if (this.selectedHero === hero) {
-      this.selectedHero = null;
-    } else {
-      this.selectedHero = hero;
-    }
-    console.log(hero);
-  }
-
-  // getHeroes(): void {
-  //   this.heroes = this.heroService.getHeroes();
-  // }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
