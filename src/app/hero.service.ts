00618c37fe07502f6f5ae56f35b1468b4dbd5f29
Khalal Walker <khalal.walker@gmail.com>
2019-01-28 16:19:10 -0800

    add update hero functionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,9 +8,14 @@ import { Hero } from './hero';
 // import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 @Injectable({ providedIn: 'root' })
 export class HeroService {
   private heroesUrl = 'api/heroes';
+
   constructor(
     private http: HttpClient,
     private messageService: MessageService
@@ -30,6 +35,13 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+  // Put: update the hero on the server
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
 
